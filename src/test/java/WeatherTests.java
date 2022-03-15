de45865eabe20608ffe436342043045541cd076f
Christian Papauschek <christian@journiapp.com>
2022-03-15 16:38:56 +0100

    initial commit

---
+++ src/test/java/WeatherTests.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class WeatherTests {
+
+    @Test
+    void addition() {
+        assertEquals(2, 2);
+    }
+
+}
